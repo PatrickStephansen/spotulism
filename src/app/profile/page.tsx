efea38bd34c2ec7085ed14f525934bffb388d68a
Patrick Stephansen <patrick.stephansen@standardbank.co.za>
2023-06-28 10:41:10 +0200

    show profile picture on profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,13 @@
 import { redirect } from "next/navigation";
 import { fetchFromSpotify } from "../lib/fetch-from-spotify";
+import Image from "next/image";
 
 export default async function Profile() {
   const profileResponse = await fetchFromSpotify('/v1/me');
   if (profileResponse.status===401){
     redirect("/api/login")
   }
+  const profile = await profileResponse.json()
 
   return (
     <div>
@@ -13,8 +15,9 @@ export default async function Profile() {
       <pre>
         profile retrieved:
 
-        {JSON.stringify(await profileResponse.json(), null, 2)}
+        {JSON.stringify(profile, null, 2)}
       </pre>
+      <Image src={profile.images?.[0]?.url} alt="user profile image" width={200} height={200}/>
     </div>
   );
 }
